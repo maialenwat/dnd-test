7a0a7abbb380219dea07d416f12d6f21dded40ef
maialenwat <maialen.watrigant@gmail.com>
2022-03-14 20:16:11 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/dnd-test/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        }
     }
-  }
 })
